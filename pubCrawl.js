09bbbedc9f6d706e80aa01c1508c73e8017543a9
Patrick Chesser <patrick.chesser@avalara.com>
2018-05-22 20:58:51 -0600

    updating drinks

--- pubCrawl.js
+++ pubCrawl.js
@@ -9,7 +9,7 @@ function Stop(name, img, drinks, shooters, game) {
 function HomeViewModel() {
 	var self = this;
 	self.stops = ko.observableArray([
-		new Stop('Venetian', './venetian.jpg', ['negroni', 'chanpagne'], [Globals.Shooters.Sam, Globals.Shooters.Brian]),
+		new Stop('Venetian', './venetian.jpg', ['champagne', 'negroni'], [Globals.Shooters.Sam, Globals.Shooters.Brian]),
 		new Stop('Mirage', './mirage.jpg', ['desert sunrise', 'beer'], [Globals.Shooters.Justen, Globals.Shooters.Pat]),
 		new Stop('Caesars Palace', './caesarsPalace.jpg', ['duck fart', 'beer'], [Globals.Shooters.Matt, Globals.Shooters.Tom]),
 		new Stop('Bellagio', './bellagio.jpg', ['boulivardier', 'beer'], [Globals.Shooters.Justin, Globals.Shooters.Chris])
