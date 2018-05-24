f3dd0835fa6ef5df1621f14dbcade6f9d1a44d8d
Patrick Chesser <patrick.chesser@avalara.com>
2018-05-23 21:09:10 -0600

    adding drinks

---
+++ drinks.js
@@ -0,0 +1,17 @@
+function Drink(name, description, picture) {
+    var selt = this;
+    self.name = name;
+    self.description = description;
+    self.picture = picture;
+};
+
+var bellini = new Drink('Belinni', "Because they have a damn Belinni Bar. Don't act like you're too good for it", "./Belinni.jpg");
+var whiskey = new Drink('Whiskey Drink', "Time to man up and make up for that Belinni. Can be taken straight, or as part of a mixed drink.", "./Whiskey.jpg");
+var royMunson = new Drink('Roy Munson', "After one of these, we should all look this good.", "./royMunson.jpg");
+var beer = new Drink('Beer', "Beer of your choice. Feel free to use this opportunity to downshift, or speed it up with a Double IPA", "./Beer.jpg");
+var caesarsRitz = new Drink("Caesar's Ritz", "It's the classiest drink you'll have all night. ", "./CaesarsRitz.jpg");
+var duckFart = new Drink('Duck Fart', "This is what Amanda and Pat toasted with at their wedding because they ran out of champagne for the bride and groom", "./duckFart.jpg");
+
+
+
+
